f2af8e3e400150546427932d85dfef14b1ff19da
iamneveralone <fieldsawarded@naver.com>
2023-08-31 00:50:15 +0900

    chore : @ManyToOne, @OneToOne 지연로딩(LAZY) 설정

--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -8,6 +8,8 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
+import static javax.persistence.FetchType.*;
+
 @Entity
 @Table(name = "orders")
 @Getter @Setter
@@ -17,14 +19,14 @@ public class Order {
     @Column(name = "order_id")
     private Long id;
 
-    @ManyToOne // Order 입장에서는 다대일 (한 주문은 한 회원에게만 속함)
+    @ManyToOne(fetch = LAZY) // Order 입장에서는 다대일 (한 주문은 한 회원에게만 속함)
     @JoinColumn(name = "member_id") // 객체의 참조와 테이블의 외래 키를 매핑
     private Member member;
 
     @OneToMany(mappedBy = "order")
     private List<OrderItem> orderItems = new ArrayList<>();
 
-    @OneToOne
+    @OneToOne(fetch = LAZY)
     @JoinColumn(name = "delivery_id") // 연관관계의 주인 (일대일은 둘 중 아무 곳에 FK 지정 가능)
     private Delivery delivery; // 접근할 일이 많은 Order 의 delivery 를 FK 로 지정
 
