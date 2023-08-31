afa8a92db81a915c975d5e22ee524323f24f48c0
iamneveralone <fieldsawarded@naver.com>
2023-08-31 15:43:29 +0900

    chore: 회원 리포지토리 EntityManager 생성자 주입으로 변경

--- src/main/java/jpabook/jpashop/repository/MemberRepository.java
+++ src/main/java/jpabook/jpashop/repository/MemberRepository.java
@@ -1,21 +1,21 @@
 package jpabook.jpashop.repository;
 
 import jpabook.jpashop.domain.Member;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
 import java.util.List;
 
 @Repository
+@RequiredArgsConstructor
 public class MemberRepository {
 
-    @PersistenceContext
-    private EntityManager em;
+    private final EntityManager em;
 
     // 회원 등록
     public void save(Member member){
-        em.persist(member);
+        em.persist(member); // persist 되면 영속성 컨텍스트에서 객체는 PK 값을 가짐 (DB 에 들어간 시점이 아니어도!)
     }
 
     // 회원 조회
@@ -37,3 +37,5 @@ public class MemberRepository {
     } // 단일 결과를 받는 getSingleResult() 는 일치하는 것이 하나도 없거나, 2개 이상이면 Exception 을 던짐
     // service 에서 중복되는 name 이 없도록 구현한다면 getResultList() 를 사용해도 좋음
 }
+// Spring Data JPA 가 @PersistenceContext 대신에 @Autowired 를 사용할 수 있게 지원해줌
+// -> 그러면, @RequiredArgsConstructor 를 사용해서 EntityManager 를 생성자 주입으로 사용 가능
