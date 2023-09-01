81fdd91fdd8c469ff57586a2eb2c10339cf68599
iamneveralone <fieldsawarded@naver.com>
2023-09-01 17:34:14 +0900

    chore : Item 엔티티에 비즈니스 로직(addStock, removeStock) 추가

--- src/main/java/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop.domain.item;
 
 import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -24,4 +25,23 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
+    //== 비즈니스 로직 ==//
+
+    // stock 증가
+    public void addStock(int quantity){
+        this.stockQuantity += quantity;
+    }
+
+    public void removeStock(int quantity){
+        int restStock = this.stockQuantity - quantity;
+        if (restStock < 0){
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+    }
 }
+// addStock, removeStock 은 Item 도메인 스스로 해결할 수 있는 경우이므로, 도메인 안에서 처리하는 것도 좋은 방법임
+// 수많은 로직들이 있는데, 서비스에 작성해야 할지 엔티티에 작성해야 할지?
+// -> 엔티티 한 곳에서 처리가 가능하면 엔티티가 처리하면 됨 (데이터를 가지고 있는 쪽에 비즈니스 로직을 가지고 있는게 응집력 좋음)
+//    But, 엔티티 하나에서 처리할 수 있는 범위를 넘어가면 서비스에서 협업이 필요!
