965a33f1103cc506f1098612b70141d303297384
iamneveralone <fieldsawarded@naver.com>
2023-09-03 15:46:34 +0900

    chore : 주문 상품 엔티티 개발

--- src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -26,4 +26,30 @@ public class OrderItem {
     private int orderPrice; // 주문 당시의 가격 (상품 가격은 변할 수 있기 때문)
     private int count; // 주문 수량
 
+    //== 생성 메서드 ==//
+    public static OrderItem createOrderItem(Item item, int orderPrice, int count){
+        OrderItem orderItem = new OrderItem();
+        orderItem.setItem(item);
+        orderItem.setOrderPrice(orderPrice);
+        orderItem.setCount(count);
+
+        item.removeStock(count);
+        return orderItem;
+    }
+    // 생성 메서드에서 setOrder 를 하지 않은 이유?
+    // 비즈니스 로직상 Order 를 만들기 위해서 OrderItem 이 필요
+    // OrderItem 을 먼저 생성해준 다음 Order 를 생성할 때 이전에 생성한 OrderItem 을 넘기게 되는데
+    // order.createOrder() -> (내부) order.addOrderItem() -> (내부) orderItem.setOrder()
+    // -> 따라서, OrderItem 의 내부에서는 따로 setOrder() 필요X
+
+    //== 비즈니스 로직 ==//
+    public void cancel() {
+        getItem().addStock(count); // 재고 원상 복구
+    }
+
+    //== 조회 로직 ==//
+    /*주문 상품 전체 가격 조회*/
+    public int getTotalPrice() {
+        return getOrderPrice() * getCount();
+    }
 }
