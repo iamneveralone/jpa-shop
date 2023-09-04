642a90b2b80cb29539c8bf2251c610736dcea7ac
iamneveralone <fieldsawarded@naver.com>
2023-09-05 01:35:06 +0900

    feat : 회원 등록

---
+++ src/main/java/jpabook/jpashop/controller/MemberController.java
@@ -0,0 +1,46 @@
+package jpabook.jpashop.controller;
+
+import jpabook.jpashop.domain.Address;
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
+
+@Controller
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberService memberService;
+
+    @GetMapping("/members/new")
+    public String createForm(Model model){
+        model.addAttribute("memberForm", new MemberForm()); // controller 에서 view 로 넘어갈 때 데이터를 model 에 담아 전달
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(@Valid MemberForm memberForm, BindingResult result) {
+
+        if (result.hasErrors()) {
+            return "members/createMemberForm";
+        }
+
+        Address address = new Address(memberForm.getCity(), memberForm.getStreet(), memberForm.getZipcode());
+
+        Member member = new Member();
+        member.setName(memberForm.getName());
+        member.setAddress(address);
+
+        memberService.join(member);
+        return "redirect:/"; // 첫번째 페이지로 다시 넘어감
+    }
+    // 만약 MemberForm 쪽에서 오류가 발생하면 원래는 튕겨버리는데,
+    // Validate 한 다음에 BindingResult 가 있으면 오류가 BindingResult 에 담겨서 코드가 실행됨
+}
+
