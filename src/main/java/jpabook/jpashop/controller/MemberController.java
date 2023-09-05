d433bc3fa97bf2c39918c9e2cf878d805d964cee
iamneveralone <fieldsawarded@naver.com>
2023-09-05 19:07:54 +0900

    feat : 회원 목록 조회

--- src/main/java/jpabook/jpashop/controller/MemberController.java
+++ src/main/java/jpabook/jpashop/controller/MemberController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -42,5 +43,15 @@ public class MemberController {
     }
     // 만약 MemberForm 쪽에서 오류가 발생하면 원래는 튕겨버리는데,
     // Validate 한 다음에 BindingResult 가 있으면 오류가 BindingResult 에 담겨서 코드가 실행됨
-}
 
+    @GetMapping("/members")
+    public String list(Model model){
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
+    }
+}
+// API 를 만들 땐 절대 Entity 를 외부에 반환하면 안 됨 -> 꼭 DTO 사용하자!!
+// API 는 스펙이다. 만약에 Entity 에 password 필드를 추가
+// -> 1. password 가 그대로 노출되는 문제 발생
+// -> 2. API 스펙이 변함 (불완전 API 스펙이 됨)
